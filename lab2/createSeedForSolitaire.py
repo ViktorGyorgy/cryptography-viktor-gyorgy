33071924173e3b37677bd4c85eb55ae47fae633c
ViktorGyorgy <viktorgyorgy@gmail.com>
2022-12-03 12:00:19 +0200

    added second joker to seed Solitaire seed generator

--- lab2/createSeedForSolitaire.py
+++ lab2/createSeedForSolitaire.py
@@ -2,4 +2,6 @@ import random
 
 x = [i for i in range(1, 54)]
 random.shuffle(x)
+#-53 a fekete joker
+x.insert(random.randint(0, len(x)), -53)
 print(x)
--- lab2/solitaire.py
+++ lab2/solitaire.py
@@ -1,4 +1,12 @@
+
+
+def moveJokers(seed : list[int]):
+  return 0;
+
 def createKey(seed : list[int]):
   #first step: search for white joker
+  print(seed)
 
   return 0
+
+createKey([32, 14, 10, 45, 6, 44, 49, 8, 11, 33, 12, 23, 9, 35, -53, 20, 30, 16, 24, 7, 52, 38, 29, 1, 17, 4, 3, 40, 18, 41, 26, 34, 43, 25, 42, 51, 48, 21, 47, 27, 53, 50, 22, 5, 31, 39, 36, 46, 28, 37, 13, 2, 15, 19])
